325c21c04c8e280f218ceb9a307ceb88110cf7ee
DeepakDinkar <ddeepu794@gmail.com>
2023-10-28 19:20:06 +0530

    Added rough article changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,29 @@ import "./App.css";
 function App() {
   const appEnvironment: string = import.meta.env.VITE_TESTING ?? "localhost";
 
-  console.log(import.meta.env);
+  const articles = [...new Array(15)];
+
+  console.log(appEnvironment);
 
   return (
-    <div className="wrapper">
-      Environment: <span>{appEnvironment}</span>
+    <div className="container">
+      <header>Header</header>
+      <main>
+        <aside>Side bar</aside>
+        <section>
+          {articles.map((_, index) => (
+            <article key={index}>
+              <div className="card-img"></div>
+              <div className="card-content">
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
+            </article>
+          ))}
+        </section>
+      </main>
+      <footer>Footer</footer>
     </div>
   );
 }
